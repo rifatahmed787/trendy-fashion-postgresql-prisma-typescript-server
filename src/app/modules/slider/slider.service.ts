3a4d6ce85cb9863f9e2b69aab7d019a285804bd7
Md Rifat <mdrifatahmed787@gmail.com>
2024-07-07 18:46:07 +0600

    slider update service

--- src/app/modules/slider/slider.service.ts
+++ src/app/modules/slider/slider.service.ts
@@ -28,7 +28,44 @@ const getSlider = async (): Promise<slider[]> => {
   return getData
 }
 
+const update_slider = async (
+  id: string,
+  slider_data: Partial<slider>,
+  user: JwtPayload
+): Promise<slider | null> => {
+  // Check if the user is an admin or has the necessary permissions
+  if (user.role !== 'ADMIN') {
+    throw new ApiError(
+      httpStatus.FORBIDDEN,
+      'You do not have permission to update slider'
+    )
+  }
+
+  const existingSliderId = parseInt(id)
+  const existingSlider = await prisma.slider.findUnique({
+    where: {
+      id: existingSliderId,
+    },
+  })
+
+  if (!existingSlider) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'slider not found')
+  }
+
+  // Now, update the slider
+  const updateSliderId = parseInt(id)
+  const updated_slider_data = await prisma.slider.update({
+    where: {
+      id: updateSliderId,
+    },
+    data: slider_data,
+  })
+
+  return updated_slider_data
+}
+
 export const SliderService = {
   create_slider,
   getSlider,
+  update_slider,
 }
