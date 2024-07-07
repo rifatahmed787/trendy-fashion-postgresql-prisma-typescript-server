7a26873eb711d68cf1b1e07023560e8c4432dbac
Md Rifat <mdrifatahmed787@gmail.com>
2024-07-07 18:39:12 +0600

    order service pagination

--- src/app/modules/order/order.services.ts
+++ src/app/modules/order/order.services.ts
@@ -2,25 +2,46 @@ import { CartProduct, PrismaClient, Role } from '@prisma/client'
 import httpStatus from 'http-status'
 import { JwtPayload } from 'jsonwebtoken'
 import ApiError from '../../errors/ApiError'
+import { pagination_map } from '../../../helpers/pagination'
+import { IPagination } from '../../../interfaces/pagination'
 const prisma = new PrismaClient()
 
-const getAllOrder = async (user: JwtPayload): Promise<CartProduct[]> => {
+const getAllOrder = async (
+  pagination_data: Partial<IPagination>,
+  user: JwtPayload
+): Promise<{
+  meta: { page: number; limit: number; total: number }
+  data: CartProduct[]
+}> => {
   if (user?.role !== Role.ADMIN) {
     throw new ApiError(
       httpStatus.FORBIDDEN,
       'Only admin users can create products'
     )
   }
+  const { page, limit, skip, sortObject } = pagination_map(pagination_data)
+
+  const total = await prisma.cartProduct.count()
   const getOrder = await prisma.cartProduct.findMany({
     where: {
       orderStatus: true,
     },
+    orderBy: sortObject,
+    skip: skip,
+    take: limit,
     include: {
       product: true,
       user: true,
     },
   })
-  return getOrder
+  return {
+    meta: {
+      page: page,
+      limit: limit,
+      total: total,
+    },
+    data: getOrder,
+  }
 }
 
 const getOrderProductByUser = async (
