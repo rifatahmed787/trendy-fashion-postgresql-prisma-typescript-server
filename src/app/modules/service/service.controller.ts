e7f21ab5216b58e84a4875b09f640120aa88b3c0
Md Rifat <mdrifatahmed787@gmail.com>
2024-07-09 00:41:27 +0600

    service service

---
+++ src/app/modules/service/service.controller.ts
@@ -0,0 +1,49 @@
+import httpStatus from 'http-status'
+import { Request, Response } from 'express'
+import catchAsync from '../../../shared/catchAsync'
+import sendResponse from '../../../shared/sendResponse'
+import { ServiceService } from './service.serivce'
+
+// Create service
+const createService = catchAsync(async (req: Request, res: Response) => {
+  const { ...service_data } = req.body
+  const user_data = req.logged_in_user
+  const result = await ServiceService.create_service(service_data, user_data)
+
+  sendResponse(res, {
+    status_code: httpStatus.OK,
+    success: true,
+    data: result,
+    message: 'Service created successfully',
+  })
+})
+
+const getService = catchAsync(async (req: Request, res: Response) => {
+  const result = await ServiceService.getService()
+  sendResponse(res, {
+    status_code: httpStatus.OK,
+    success: true,
+    data: result,
+    message: 'Service retrieved successfully',
+  })
+})
+
+const updateService = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params
+  const user = req.logged_in_user
+  const { ...service_data } = req.body
+  const result = await ServiceService.update_service(id, service_data, user)
+
+  sendResponse(res, {
+    status_code: httpStatus.OK,
+    success: true,
+    data: result,
+    message: 'Service updated successfully',
+  })
+})
+
+export const ServiceController = {
+  createService,
+  getService,
+  updateService,
+}
---
+++ src/app/modules/service/service.serivce.ts
@@ -0,0 +1,71 @@
+import { PrismaClient, Role, service } from '@prisma/client'
+import { JwtPayload } from 'jsonwebtoken'
+import ApiError from '../../errors/ApiError'
+import httpStatus from 'http-status'
+
+const prisma = new PrismaClient()
+
+const create_service = async (
+  service_data: service,
+  user: JwtPayload
+): Promise<service | null> => {
+  // Check if the user is an admin
+  if (user?.role !== Role.ADMIN) {
+    throw new ApiError(
+      httpStatus.FORBIDDEN,
+      'Only admin users can create service'
+    )
+  }
+
+  const createService = await prisma.service.create({
+    data: service_data,
+  })
+  return createService
+}
+
+const getService = async (): Promise<service[]> => {
+  const getData = await prisma.service.findMany()
+  return getData
+}
+
+const update_service = async (
+  id: string,
+  service_data: Partial<service>,
+  user: JwtPayload
+): Promise<service | null> => {
+  // Check if the user is an admin or has the necessary permissions
+  if (user.role !== 'ADMIN') {
+    throw new ApiError(
+      httpStatus.FORBIDDEN,
+      'You do not have permission to update service'
+    )
+  }
+
+  const existingServiceId = parseInt(id)
+  const existingService = await prisma.service.findUnique({
+    where: {
+      id: existingServiceId,
+    },
+  })
+
+  if (!existingService) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'service not found')
+  }
+
+  // Now, update the service
+  const updateServiceId = parseInt(id)
+  const updated_service_data = await prisma.service.update({
+    where: {
+      id: updateServiceId,
+    },
+    data: service_data,
+  })
+
+  return updated_service_data
+}
+
+export const ServiceService = {
+  create_service,
+  getService,
+  update_service,
+}
