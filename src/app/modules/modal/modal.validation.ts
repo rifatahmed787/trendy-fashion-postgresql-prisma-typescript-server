4d3c4e0dc9d4dfb9cadd224f6e418b53b951f62f
Md Rifat <mdrifatahmed787@gmail.com>
2024-07-25 11:52:56 +0600

    modal updated

--- src/app/modules/modal/modal.validation.ts
+++ src/app/modules/modal/modal.validation.ts
@@ -1,45 +1,64 @@
 import { z } from 'zod'
 
 // Schema for creating a ModalImg
-export const createModalImgSchema = z
-  .object({
-    img: z.string().url({ message: 'Image must be a valid URL' }),
-    percent: z.number({ required_error: 'Percentage is required' }),
-    categoryName: z.string({ required_error: 'Category name must be given' }),
-    startTime: z.date().optional(),
-    endTime: z.date().optional(),
-  })
-  .refine(
-    data => {
-      if (data.startTime && data.endTime) {
-        return data.endTime > data.startTime
+export const createModalImgSchema = z.object({
+  body: z
+    .object({
+      img: z.string().url({ message: 'Image must be a valid URL' }),
+      percent: z.number({ required_error: 'Percentage is required' }),
+      categoryName: z.string({ required_error: 'Category name must be given' }),
+      startTime: z.string().refine(val => !isNaN(Date.parse(val)), {
+        message: 'Start Date must be a valid DateTime',
+      }),
+      endTime: z.string().refine(val => !isNaN(Date.parse(val)), {
+        message: 'End Date must be a valid DateTime',
+      }),
+    })
+    .refine(
+      data => {
+        const startTime = new Date(data.startTime)
+        const endTime = new Date(data.endTime)
+        return endTime > startTime
+      },
+      {
+        message: 'End Date must be after Start Date',
+        path: ['endDate'],
       }
-      return true
-    },
-    {
-      message: 'End time must be after start time',
-      path: ['endTime'],
-    }
-  )
+    ),
+})
 
 // Schema for updating a ModalImg
-export const updateModalImgSchema = z
-  .object({
-    img: z.string().url({ message: 'Image must be a valid URL' }).optional(),
-    percent: z.number().optional(),
-    categoryName: z.string().optional(),
-    startTime: z.date().optional(),
-    endTime: z.date().optional(),
-  })
-  .refine(
-    data => {
-      if (data.startTime && data.endTime) {
-        return data.endTime > data.startTime
+export const updateModalImgSchema = z.object({
+  body: z
+    .object({
+      img: z.string().url({ message: 'Image must be a valid URL' }).optional(),
+      percent: z.number().optional(),
+      categoryName: z.string().optional(),
+      startTime: z
+        .string()
+        .refine(val => !isNaN(Date.parse(val)), {
+          message: 'Start Date must be a valid DateTime',
+        })
+        .optional(),
+      endTime: z
+        .string()
+        .refine(val => !isNaN(Date.parse(val)), {
+          message: 'End Date must be a valid DateTime',
+        })
+        .optional(),
+    })
+    .refine(
+      data => {
+        if (data.startTime && data.endTime) {
+          const startTime = new Date(data.startTime)
+          const endTime = new Date(data.endTime)
+          return endTime > startTime
+        }
+        return true
+      },
+      {
+        message: 'End Date must be after Start Date',
+        path: ['endDate'],
       }
-      return true
-    },
-    {
-      message: 'End time must be after start time',
-      path: ['endTime'],
-    }
-  )
+    ),
+})
