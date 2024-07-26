a5d74cda20b04b9a8acbc6180d783e7ee3eb0b1d
Md Rifat <mdrifatahmed787@gmail.com>
2024-07-26 22:11:01 +0600

    type controller

---
+++ src/app/modules/productType/productType.controller.ts
@@ -0,0 +1,67 @@
+import httpStatus from 'http-status'
+import { Request, Response } from 'express'
+import catchAsync from '../../../shared/catchAsync'
+import sendResponse from '../../../shared/sendResponse'
+import { ProductTypeService } from './productType.service'
+
+// Create Type
+const createType = catchAsync(async (req: Request, res: Response) => {
+  const { ...Type_data } = req.body
+  const user_data = req.logged_in_user
+  const result = await ProductTypeService.createProductType(
+    Type_data,
+    user_data
+  )
+
+  sendResponse(res, {
+    status_code: httpStatus.OK,
+    success: true,
+    data: result,
+    message: 'Type created successfully',
+  })
+})
+
+const getType = catchAsync(async (req: Request, res: Response) => {
+  const user = req.logged_in_user
+  const result = await ProductTypeService.getType(user)
+  sendResponse(res, {
+    status_code: httpStatus.OK,
+    success: true,
+    data: result,
+    message: 'Type retrieved successfully',
+  })
+})
+
+const updateType = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params
+  const user = req.logged_in_user
+  const { ...Type_data } = req.body
+  const result = await ProductTypeService.updateType(id, Type_data, user)
+
+  sendResponse(res, {
+    status_code: httpStatus.OK,
+    success: true,
+    data: result,
+    message: 'Type updated successfully',
+  })
+})
+
+const deleteType = catchAsync(async (req: Request, res: Response) => {
+  const user = req.logged_in_user
+  const { id } = req.params
+  const result = await ProductTypeService.deleteType(id, user)
+
+  sendResponse(res, {
+    status_code: httpStatus.OK,
+    success: true,
+    data: result,
+    message: 'Type deleted successfully',
+  })
+})
+
+export const ProductTypeController = {
+  createType,
+  getType,
+  updateType,
+  deleteType,
+}
